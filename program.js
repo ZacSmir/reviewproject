b2870e67bde4bb2276b848d51907f766ff96ead6
ZacSmir <Zach@smirin.us>
2016-10-10 09:29:33 -0700

    Classadoo autocommit

--- program.js
+++ program.js
@@ -1,20 +1,20 @@
-//function doingthings() {
-//console.log("SO MANY PENGUINS")
-//
-//}
-//
-//
-//$("#dothing").click(doingthings){
-//}
+function doingthings() {
+console.log("SO MANY PENGUINS")
 
-function whenClicked() {
-  console.log("function run")
-  return "1111"
 }
 
+
+$("#dothing").click(doingthings){
+}
+
+//function whenClicked() {
+//  console.log("function run")
+//  return "1111"
+//}
+//
 //var a = whenClicked()
-var b = whenClicked
+//var b = whenClicked
 //
 //console.log(a.toString())
 //console.log(b.toString())
-b()
+//b()
